abb251a71c68430349a511b690eb6134f02a025d
cxzhang <chenxizhang@theinitium.com>
2017-11-28 23:35:38 +0800

    bump to react 16

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+// React 16 global polyfill for IE < 11
+import 'core-js/es6/map'
+import 'core-js/es6/set'
+
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import store from '~/store'
 
+import store from '~/store'
 import App from './App'
 
 ReactDOM.render(
